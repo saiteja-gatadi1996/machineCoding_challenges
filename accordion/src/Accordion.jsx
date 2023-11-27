1c270c8cb0bc138e28766adbc4f519a079052434
saiteja-gatadi1996 <saitejagatadi543@gmail.com>
2023-11-27 23:21:01 +0530

    accordion

---
+++ accordion/src/Accordion.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+
+const AccordionItem = ({ id, title, info, toggleAccordion, isActive }) => {
+  // every time you click on + or - button, we are sending that particular clicked accordion component id to the function (which is in the parent component ex; App.jsx)
+  const handleClick = () => {
+    //id is part of the data.js
+    toggleAccordion(id);
+  };
+
+  return (
+    <div className='accordion-container'>
+      <h2>{title}</h2>
+      <button onClick={handleClick} aria-expanded={isActive}>
+        {isActive ? '-' : '+'}
+      </button>
+      {isActive && <p>{info}</p>}
+    </div>
+  );
+};
+
+AccordionItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  info: PropTypes.string.isRequired,
+  toggleAccordion: PropTypes.func.isRequired,
+  isActive: PropTypes.bool.isRequired,
+};
+
+export default AccordionItem;
---
+++ accordion/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState, useCallback } from 'react';
+import AccordionItem from './Accordion';
+import data from './data';
+
+const App = () => {
+  // state to maintain checkbox
+  const [allowMultipleOpen, setAllowMultipleOpen] = useState(true);
+  // state to maintain the active/opened accordions
+  const [activeAccordions, setActiveAccordions] = useState(new Set());
+
+  const handleCheckboxChange = useCallback(() => {
+    //below state is to simply toggle the checkbox (checked/un-checked)
+    setAllowMultipleOpen((prev) => !prev);
+  }, []);
+
+  //below function is to toggle the accordion component itself,
+  // this function runs every time you toggle the checkbox
+  const toggleAccordion = useCallback(
+    (id) => {
+      //activeAccordions means the ones that were opened
+      setActiveAccordions((prev) => {
+        //at first, we are storing them in the Set
+        const updatedActiveAccordions = new Set(prev);
+        //if the id of the accordion you are going to click now is already in opened ?
+        if (updatedActiveAccordions.has(id)) {
+          // if yes, then delete that id from that set stored (which means, the accordion is going to be closed)
+          updatedActiveAccordions.delete(id);
+        } else {
+          // if that id is not part + checkbox is not clicked
+          if (!allowMultipleOpen) {
+            // simply maintain an empty set by clearing everything (this helps us to close everything that is opened only when checkbox is unchecked)
+            updatedActiveAccordions.clear();
+          }
+          // if that id is not opened at all + checkbox is not clicked + then you have to add that into the set
+          // below is the default condition or straight away condition
+          updatedActiveAccordions.add(id);
+        }
+        // returning the set
+        return updatedActiveAccordions;
+      });
+    },
+    [allowMultipleOpen]
+  );
+
+  return (
+    <div>
+      <label htmlFor='multiple-open'>Allow multiple accordions open?</label>
+      <input
+        type='checkbox'
+        id='multiple-open'
+        checked={allowMultipleOpen}
+        onChange={handleCheckboxChange}
+      />
+      {data.map((item) => (
+        <AccordionItem
+          key={item.id}
+          //below line of `.has` helps us to toggle the info
+          isActive={activeAccordions.has(item.id)}
+          toggleAccordion={toggleAccordion}
+          {...item}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default App;
