08180e81d0fd184ce1ccfd8b3f1817159f78da1a
saiteja-gatadi1996 <saitejagatadi543@gmail.com>
2023-12-01 00:08:55 +0530

    Light and dark

--- 10_light and dark mode/src/App.jsx
+++ 10_light and dark mode/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Form from './components/Form';
+import { createContext, useState } from 'react';
+
+export const ThemeContext = createContext(null);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState('dark');
+
+  const toggleTheme = () => {
+    setTheme((currentTheme) => (currentTheme === 'dark' ? 'light' : 'dark'));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className='App' id={theme}>
+        <Form />
+        <div className='switch'>
+          <label> {theme === 'light' ? 'Light Mode' : 'Dark Mode'}</label>
+          <button
+            onClick={toggleTheme}
+            className={`switch-button ${
+              theme === 'dark' ? 'switch-button-on' : ''
+            }`}
+          >
+            <span className='slider'></span>
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ThemeContext.Provider>
+  );
 }
 
-export default App
+export default App;
