dceb0dd733e060486ca41ff6e5fb714afaa78cc9
saiteja-gatadi1996 <saitejagatadi543@gmail.com>
2023-11-30 23:26:10 +0530

    json creator

--- 09_jsonCreator/src/App.jsx
+++ 09_jsonCreator/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback } from 'react';
+import JSONCreator from './components/JSONCreator';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [jsonStructure, setJsonStructure] = useState({});
+  const [jsonOutput, setJsonOutput] = useState('');
+
+  const handleDataChange = useCallback((id, key, value, children) => {
+    setJsonStructure({ [key]: value, children });
+  }, []);
+
+  const handleGetJSON = () => {
+    setJsonOutput(JSON.stringify(jsonStructure, null, 2));
+  };
 
   return (
-    <>
+    <div className='container'>
+      <JSONCreator
+        id={0}
+        data={jsonStructure}
+        onDataChange={handleDataChange}
+      />
+      <button className='get-json-button' onClick={handleGetJSON}>
+        Get JSON
+      </button>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <textarea readOnly value={jsonOutput} rows='10' cols='50' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
