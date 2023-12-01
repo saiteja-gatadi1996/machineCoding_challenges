e26576457194680430d853a4fa47c04ee41d5b08
saiteja-gatadi1996 <saitejagatadi543@gmail.com>
2023-12-01 10:27:08 +0530

    paginator

--- 12_paginator/src/App.jsx
+++ 12_paginator/src/App.jsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Posts from './components/Posts';
+import Pagination from './components/Pagination';
+import './App.css';
+import useFetch from './customHooks/useCachedApi';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const { data: posts, loading } = useFetch(
+    'https://jsonplaceholder.typicode.com/posts'
+  );
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+  let currentPosts = [];
+
+  // Get current posts
+  if (!loading && posts?.length > 0) {
+    // currentPage starts from 1
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    currentPosts = posts?.slice(indexOfFirstPost, indexOfLastPost);
+  }
+
+  // Change page
+  const paginate = (pageNumber) => {
+    if (pageNumber) {
+      setCurrentPage(pageNumber);
+    }
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className='container'>
+      <h1 className='text-primary'>My Blog</h1>
+      {posts?.length > 0 && (
+        <>
+          <Posts posts={currentPosts} loading={loading} />
+          <Pagination
+            postsPerPage={postsPerPage}
+            totalPosts={posts?.length}
+            paginate={paginate}
+          />
+        </>
+      )}
+    </div>
+  );
+};
 
-export default App
+export default App;
