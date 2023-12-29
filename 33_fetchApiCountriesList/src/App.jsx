ceab26d72d056c3c9b0c4f5b1b7bd91945bcadd8
saiteja-gatadi1996 <saitejagatadi543@gmail.com>
2023-12-29 08:19:03 +0530

    fetchAPI and display capitals of countries + filter logic

---
+++ 33_fetchApiCountriesList/src/App.jsx
@@ -0,0 +1,13 @@
+// App.js
+import React, { useState, useEffect } from 'react';
+import CountriesPage from './CountriesPage';
+
+function App() {
+  return (
+    <div className='App'>
+      <CountriesPage />
+    </div>
+  );
+}
+
+export default App;
